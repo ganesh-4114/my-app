d2cb456233c88e78f265aae3b9a556597046aae3
Ganesh Chowdary Dandamudi <ganeshdandamudi16@gmail.com>
2022-04-06 18:41:10 +0530

    updated

---
+++ src/app/ganesh.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component ({
+    selector: 'app-dgf',
+    template: '<h1>Hello World</h1>',
+    styles: ['h1 { font-weight: normal; }']
+})
+
+export class GaneshComponent {
+
+}
